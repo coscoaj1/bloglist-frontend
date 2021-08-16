9f80cc03fedd9000c8835e4d2428a3b34d08ec8f
Anthony Cosco <coscoanthony1@gmail.com>
2021-08-16 06:55:47 -0400

    added login

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import React, { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import React, { useState, useEffect } from 'react';
+import Blog from './components/Blog';
+import blogService from './services/blogs';
+import loginService from './services/login';
+import LoginForm from './components/LoginForm';
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+	const [blogs, setBlogs] = useState([]);
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+	const [user, setUser] = useState(null);
+	const [errorMessage, setErrorMessage] = useState(null);
 
-  useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+	useEffect(() => {
+		blogService.getAll().then((blogs) => setBlogs(blogs));
+	}, []);
 
-  return (
-    <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
-  )
-}
+	const handleLogin = async (event) => {
+		event.preventDefault();
+		console.log('logging in with', username, password);
 
-export default App
+		try {
+			const user = await loginService.login({
+				username,
+				password,
+			});
+
+			setUser(user);
+			setUsername('');
+			setPassword('');
+		} catch (exception) {
+			setErrorMessage('Wrong credentials');
+			setTimeout(() => {
+				setErrorMessage(null);
+			}, 5000);
+		}
+	};
+
+	const handleNameChange = (event) => {
+		setUsername(event.target.value);
+	};
+	const handlePasswordChange = (event) => {
+		setPassword(event.target.value);
+	};
+
+	return (
+		<div>
+			<h2>Login to Blogs</h2>
+			{user === null ? (
+				<LoginForm
+					username={username}
+					password={password}
+					handleNameChange={handleNameChange}
+					handlePasswordChange={handlePasswordChange}
+					handleLogin={handleLogin}
+				/>
+			) : (
+				<div>
+					<h2>blogs</h2>
+					{blogs.map((blog) => (
+						<Blog key={blog.id} blog={blog} />
+					))}
+				</div>
+			)}
+		</div>
+	);
+};
+
+export default App;
