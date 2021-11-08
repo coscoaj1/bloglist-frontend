eaef3133efe3df3720023811d16ac0b14cda9c31
Anthony Cosco <coscoanthony1@gmail.com>
2021-11-08 06:28:58 -0500

    hero working

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import './Index.css';
 import ErrorMessage from './components/Error';
 import { Typography, Container } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core';
+import Hero from './components/Hero';
 
 import {
 	BrowserRouter as Router,
@@ -147,7 +148,8 @@ const App = () => {
 					<div className={classes.root}>
 						<Notification message={notificationMessage} />
 
-						<Typography variant="h2">Cat Blogs app</Typography>
+						<Typography variant="h2">BlogShare</Typography>
+						<Hero />
 						<Switch>
 							<Route path="/users/:id">
 								<User users={users} />
