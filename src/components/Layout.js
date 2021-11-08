4b529a08b848b4d76cc78cfe42e4be826c1c59e0
Anthony Cosco <coscoanthony1@gmail.com>
2021-11-08 17:10:53 -0500

    pic

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,10 +3,8 @@ import { makeStyles } from '@material-ui/core';
 
 const useStyles = makeStyles({
 	root: {
-		background: 'white',
 		width: '100%',
 		height: '100%',
-		color: 'black',
 		display: 'flex',
 		alignItems: 'center',
 	},
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,29 +6,29 @@ import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { Link } from 'react-router-dom';
 import { makeStyles, Typography } from '@material-ui/core';
 import Avatar from '@material-ui/core/Avatar';
-import ok from '../ok.png'
+import ok from '../ok.png';
 
 const useStyles = makeStyles({
 	root: {
 		display: 'flex',
+		color: 'white',
 	},
 	orange: {
 		height: '60px',
 		width: '60px',
-		margin: '-.3em'
+		margin: '-.3em',
+		color: 'white',
 	},
 	toolLeft: {
-		width: '100%'
+		width: '100%',
+		color: 'white',
 	},
 	toolRight: {
 		display: 'flex',
 		width: '30%',
-		gap: '1em'
-	
+		gap: '1em',
+		color: 'white',
 	},
-
-
-
 });
 export default function NavBar({ user, handleLogout }) {
 	const classes = useStyles();
@@ -37,47 +37,41 @@ export default function NavBar({ user, handleLogout }) {
 
 	return (
 		<div>
-			<AppBar  position="static">
-					{isMatch ? (
-						<DrawerComponent handleLogout={handleLogout} user={user} />
-						) : (
-							<Toolbar className={classes.root}>
-						
-							<div className={classes.toolLeft}>
-								<Button component={Link} to="/">
-									home
+			<AppBar position="static">
+				{isMatch ? (
+					<DrawerComponent handleLogout={handleLogout} user={user} />
+				) : (
+					<Toolbar className={classes.root}>
+						<div className={classes.toolLeft}>
+							<Button component={Link} to="/">
+								<Typography>home</Typography>
+							</Button>
+							<Button component={Link} to="/blogs">
+								blogs
+							</Button>
+							<Button className={classes.users} component={Link} to="/users">
+								users
+							</Button>
+						</div>
+						{user ? (
+							<div className={classes.toolRight}>
+								<Button
+									size="small"
+									onClick={handleLogout}
+									className={classes.user}
+								>
+									logout
 								</Button>
-								<Button component={Link} to="/blogs">
-									blogs
-								</Button>
-								<Button className={classes.users} component={Link} to="/users">
-									users
-								</Button>
+								<Typography>{user.name}</Typography>
+								<Avatar className={classes.orange} alt="OK" src={ok} />
 							</div>
-							{user ? (
-								
-									
-									<div className={classes.toolRight}>
-										<Button
-											size="small"
-											onClick={handleLogout}
-											className={classes.user}
-											>
-											logout
-										</Button>
-											<Typography>{user.name}</Typography>
-											<Avatar className={classes.orange} alt="OK" src={ok} />
-										
-									</div>
-										
-							) : (
-								<Button color="inherit" component={Link} to="/login">
-									login
-								</Button>
-							)}
-						
-				</Toolbar>
-					)}
+						) : (
+							<Button color="inherit" component={Link} to="/login">
+								login
+							</Button>
+						)}
+					</Toolbar>
+				)}
 			</AppBar>
 		</div>
 	);
