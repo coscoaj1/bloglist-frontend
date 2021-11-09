b3b2cbb7805d57fe0e756822e4e0f55613192540
coscoaj1 <coscoanthony1@gmail.com>
2021-11-09 18:43:57 -0500

    appbar

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import React, { useState, useEffect } from 'react';
-import blogService from './services/blogs';
-import loginService from './services/login';
-import userService from './services/users';
-import LoginForm from './components/LoginForm';
-import BlogForm from './components/BlogForm';
-import Blogs from './components/Blogs';
-import Layout from './components/Layout';
-import Blog from './components/Blog';
-import Notification from './components/Notification';
-import Users from './components/Users';
-import User from './components/User';
-import './Index.css';
-import ErrorMessage from './components/Error';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import Hero from './components/Hero';
+import React, { useState, useEffect } from "react";
+import blogService from "./services/blogs";
+import loginService from "./services/login";
+import userService from "./services/users";
+import LoginForm from "./components/LoginForm";
+import BlogForm from "./components/BlogForm";
+import Blogs from "./components/Blogs";
+import Layout from "./components/Layout";
+import Blog from "./components/Blog";
+import Notification from "./components/Notification";
+import Users from "./components/Users";
+import User from "./components/User";
+import "./Index.css";
+import ErrorMessage from "./components/Error";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Hero from "./components/Hero";
 
 import {
-	BrowserRouter as Router,
-	Switch,
-	Route,
-	Redirect,
-} from 'react-router-dom';
-import NavBar from './components/NavBar';
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import NavBar from "./components/NavBar";
 
 // const useStyles = makeStyles({
 // 	root: {
@@ -31,169 +31,168 @@ import NavBar from './components/NavBar';
 // });
 
 const App = () => {
-	const [blogs, setBlogs] = useState([]);
-	const [username, setUsername] = useState('');
-	const [password, setPassword] = useState('');
-	const [user, setUser] = useState(null);
-	const [errorMessage, setErrorMessage] = useState(null);
-	const [notificationMessage, setNotificationMessage] = useState(null);
-	const [users, setUsers] = useState([]);
+  const [blogs, setBlogs] = useState([]);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [notificationMessage, setNotificationMessage] = useState(null);
+  const [users, setUsers] = useState([]);
 
-	useEffect(() => {
-		blogService.getAll().then((blogs) => setBlogs(blogs));
-	}, []);
+  useEffect(() => {
+    blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, []);
 
-	useEffect(() => {
-		userService.getUsers().then((users) => setUsers(users));
-	}, []);
+  useEffect(() => {
+    userService.getUsers().then((users) => setUsers(users));
+  }, []);
 
-	useEffect(() => {
-		const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser');
-		if (loggedUserJSON) {
-			const user = JSON.parse(loggedUserJSON);
-			setUser(user);
-			blogService.setToken(user.token);
-		}
-	}, []);
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem("loggedBlogappUser");
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+      blogService.setToken(user.token);
+    }
+  }, []);
 
-	const handleLogin = async (event) => {
-		event.preventDefault();
-		try {
-			const user = await loginService.login({
-				username,
-				password,
-			});
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    try {
+      const user = await loginService.login({
+        username,
+        password,
+      });
 
-			window.localStorage.setItem('loggedBlogappUser', JSON.stringify(user));
-			blogService.setToken(user.token);
-			setUser(user);
-			setUsername('');
-			setPassword('');
-		} catch (exception) {
-			setErrorMessage('Invalid username or password.');
-			setTimeout(() => {
-				setErrorMessage(null);
-			}, 5000);
-		}
-	};
+      window.localStorage.setItem("loggedBlogappUser", JSON.stringify(user));
+      blogService.setToken(user.token);
+      setUser(user);
+      setUsername("");
+      setPassword("");
+    } catch (exception) {
+      setErrorMessage("Invalid username or password.");
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
+    }
+  };
 
-	const addBlog = (blogObject) => {
-		blogService //
-			.create(blogObject)
-			.then((returnedBlog) => {
-				setBlogs(blogs.concat(returnedBlog));
-				console.log(`added${returnedBlog.title}`);
-				setNotificationMessage(`Added ${returnedBlog.title}`);
-				timeout();
-			})
+  const addBlog = (blogObject) => {
+    blogService //
+      .create(blogObject)
+      .then((returnedBlog) => {
+        setBlogs(blogs.concat(returnedBlog));
+        console.log(`added${returnedBlog.title}`);
+        setNotificationMessage(`Added ${returnedBlog.title}`);
+        timeout();
+      })
 
-			.catch((error) => {
-				console.log(error.response.data);
-			});
-	};
+      .catch((error) => {
+        console.log(error.response.data);
+      });
+  };
 
-	const addComment = async (blog) => {
-		await blogService.comment(blog.id, {
-			comments: blog.comment,
-		});
-		const blogs = await blogService.getAll();
-		setBlogs(blogs);
-	};
+  const addComment = async (blog) => {
+    await blogService.comment(blog.id, {
+      comments: blog.comment,
+    });
+    const blogs = await blogService.getAll();
+    setBlogs(blogs);
+  };
 
-	const handleLike = async (blog) => {
-		await blogService.update(blog.id, {
-			title: blog.title,
-			author: blog.author,
-			url: blog.url,
-			likes: blog.likes + 1,
-		});
+  const handleLike = async (blog) => {
+    await blogService.update(blog.id, {
+      title: blog.title,
+      author: blog.author,
+      url: blog.url,
+      likes: blog.likes + 1,
+    });
 
-		const blogs = await blogService.getAll();
-		setBlogs(blogs);
-	};
+    const blogs = await blogService.getAll();
+    setBlogs(blogs);
+  };
 
-	const handleDelete = async (blog) => {
-		if (window.confirm(`Delete ${blog.title}?`)) {
-			await blogService.remove(blog.id);
+  const handleDelete = async (blog) => {
+    if (window.confirm(`Delete ${blog.title}?`)) {
+      await blogService.remove(blog.id);
 
-			let blogs = await blogService.getAll();
-			setBlogs(blogs);
+      let blogs = await blogService.getAll();
+      setBlogs(blogs);
 
-			setNotificationMessage(`Deleted ${blog.title}`);
-			timeout();
-		}
-	};
+      setNotificationMessage(`Deleted ${blog.title}`);
+      timeout();
+    }
+  };
 
-	const handleLogout = () => {
-		console.log(`logging out`, user.name);
-		setUser(null);
-		window.localStorage.clear();
-	};
+  const handleLogout = () => {
+    console.log(`logging out`, user.name);
+    setUser(null);
+    window.localStorage.clear();
+  };
 
-	const timeout = () => {
-		setTimeout(() => {
-			setNotificationMessage(null);
-		}, 5000);
-	};
+  const timeout = () => {
+    setTimeout(() => {
+      setNotificationMessage(null);
+    }, 5000);
+  };
 
-	return (
-		<Router>
-			<Layout>
-				<Container sx={{ textAlign: 'center' }}>
-					<NavBar user={user} handleLogout={handleLogout} />
-					<div>
-						<Typography variant="h2">Cat Blogs app</Typography>
-						<Hero />
-						<Switch>
-							<Route path="/users/:id">
-								<User users={users} />
-							</Route>
-							<Route path="/blogs/:id">
-								<Blog
-									handleDelete={handleDelete}
-									blogs={blogs}
-									createComment={addComment}
-									handleLike={handleLike}
-								></Blog>
-							</Route>
-							<Route path="/users">
-								<Users users={users} />
-							</Route>
-							<Route path="/blogs">
-								<Blogs
-									blogs={blogs}
-									handleLike={handleLike}
-									handleDelete={handleDelete}
-								/>
-							</Route>
-							<Route path="/login">
-								{!user ? (
-									<LoginForm
-										username={username}
-										password={password}
-										handleUserNameChange={({ target }) =>
-											setUsername(target.value)
-										}
-										handlePasswordChange={({ target }) =>
-											setPassword(target.value)
-										}
-										handleLogin={handleLogin}
-									/>
-								) : (
-									<Redirect to="/" />
-								)}
-							</Route>
-							<Route path="/">
-								{user ? <BlogForm createBlog={addBlog} /> : null}
-							</Route>
-						</Switch>
-						<Notification message={notificationMessage} />
-						<ErrorMessage message={errorMessage} />
-					</div>
-				</Container>
-			</Layout>
-		</Router>
-	);
+  return (
+    <Router>
+      <Layout>
+        <Container sx={{ textAlign: "center" }}>
+          <NavBar user={user} handleLogout={handleLogout} />
+          <div>
+            <Hero />
+            <Switch>
+              <Route path="/users/:id">
+                <User users={users} />
+              </Route>
+              <Route path="/blogs/:id">
+                <Blog
+                  handleDelete={handleDelete}
+                  blogs={blogs}
+                  createComment={addComment}
+                  handleLike={handleLike}
+                ></Blog>
+              </Route>
+              <Route path="/users">
+                <Users users={users} />
+              </Route>
+              <Route path="/blogs">
+                <Blogs
+                  blogs={blogs}
+                  handleLike={handleLike}
+                  handleDelete={handleDelete}
+                />
+              </Route>
+              <Route path="/login">
+                {!user ? (
+                  <LoginForm
+                    username={username}
+                    password={password}
+                    handleUserNameChange={({ target }) =>
+                      setUsername(target.value)
+                    }
+                    handlePasswordChange={({ target }) =>
+                      setPassword(target.value)
+                    }
+                    handleLogin={handleLogin}
+                  />
+                ) : (
+                  <Redirect to="/" />
+                )}
+              </Route>
+              <Route path="/">
+                {user ? <BlogForm createBlog={addBlog} /> : null}
+              </Route>
+            </Switch>
+            <Notification message={notificationMessage} />
+            <ErrorMessage message={errorMessage} />
+          </div>
+        </Container>
+      </Layout>
+    </Router>
+  );
 };
 
 export default App;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import Toolbar from '@mui/material/Toolbar';
-import AppBar from '@mui/material/AppBar';
-import Button from '@mui/material/Button';
-import DrawerComponent from './DrawerComponent';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
-import { Link } from 'react-router-dom';
-import Typography from '@mui/material/Typography';
-import Avatar from '@mui/material/Avatar';
-import ok from '../ok.png';
-import { useTheme } from '@mui/material/styles';
+import React from "react";
+import { styled } from "@mui/material/styles";
+import Toolbar from "@mui/material/Toolbar";
+import AppBar from "@mui/material/AppBar";
+import Button from "@mui/material/Button";
+import PestControlRodentIcon from "@mui/icons-material/PestControlRodent";
+import DrawerComponent from "./DrawerComponent";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import { Link } from "react-router-dom";
+import Typography from "@mui/material/Typography";
+import Avatar from "@mui/material/Avatar";
+import ok from "../ok.png";
+import { useTheme } from "@mui/material/styles";
+// MuiAppBar-root
 
 // const useStyles = makeStyles({
 // 	root: {
@@ -30,56 +33,73 @@ import { useTheme } from '@mui/material/styles';
 // 	},
 
 // });
-export default function NavBar({ user, handleLogout }) {
-	const theme = useTheme();
-	const isMatch = useMediaQuery(theme.breakpoints.down('xs'));
 
-	return (
-		<div>
-			<AppBar position="static">
-				<Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
-					<div>
-						<Button color="inherit" component={Link} to="/">
-							home
-						</Button>
-						<Button color="inherit" component={Link} to="/blogs">
-							blogs
-						</Button>
-						<Button
-							component="div"
-							sx={{ flexGrow: 1 }}
-							color="inherit"
-							component={Link}
-							to="/users"
-						>
-							users
-						</Button>
-					</div>
-					{user ? (
-						<Toolbar sx={{ display: 'flex' }}>
-							<Button size="small" onClick={handleLogout}>
-								logout
-							</Button>
-							<Typography>{user.name}</Typography>
-							<Avatar alt="OK" src={ok} />
-						</Toolbar>
-					) : (
-						<Button color="inherit" component={Link} to="/login">
-							login
-						</Button>
-					)}
-				</Toolbar>
-			</AppBar>
-		</div>
-	);
+export default function NavBar({ user, handleLogout }) {
+  const theme = useTheme();
+  const isMatch = useMediaQuery(theme.breakpoints.down("xs"));
+
+  return (
+    <>
+      <AppBar position="fixed" sx={{ width: "100vw", margin: 0 }}>
+        <Toolbar
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          <div>
+            <Button
+              component={Link}
+              to="/"
+              color="inherit"
+              sx={{ paddingRight: "48px" }}
+            >
+              <PestControlRodentIcon
+                fontSize="large"
+                sx={{ color: "gray", paddingRight: "12px" }}
+              />
+              <Typography variant="h4">CAT-BLOGS</Typography>
+            </Button>
+
+            <Button color="inherit" component={Link} to="/blogs">
+              blogs
+            </Button>
+            <Button
+              component="div"
+              sx={{ flexGrow: 1 }}
+              color="inherit"
+              component={Link}
+              to="/users"
+            >
+              users
+            </Button>
+          </div>
+          {user ? (
+            <Toolbar sx={{ display: "flex" }}>
+              <Button size="small" onClick={handleLogout}>
+                logout
+              </Button>
+              <Typography>{user.name}</Typography>
+              <Avatar alt="OK" src={ok} />
+            </Toolbar>
+          ) : (
+            <Button color="inherit" component={Link} to="/login">
+              login
+            </Button>
+          )}
+        </Toolbar>
+      </AppBar>
+      <Toolbar />
+    </>
+  );
 }
 
 {
-	/* {isMatch ? (
+  /* {isMatch ? (
   <DrawerComponent handleLogout={handleLogout} user={user} />
 ) : ( */
 }
 
 {
-	/* )} */
+  /* )} */
 }
