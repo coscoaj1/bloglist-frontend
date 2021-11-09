5396196e57cfabd44f64cba3a0275b15d0e35158
Anthony Cosco <coscoanthony1@gmail.com>
2021-11-09 05:43:27 -0500

    migrating

--- src/components/DrawerComponent.js
+++ src/components/DrawerComponent.js
@@ -1,118 +1,118 @@
-import { useState } from "react";
+import { useState } from 'react';
 import {
-  Drawer,
-  List,
-  ListItem,
-  ListItemText,
-  ListItemIcon,
-  IconButton,
-} from "@material-ui/core";
-import MenuIcon from "@mui/icons-material/Menu";
-import HomeIcon from "@mui/icons-material/Home";
-import PeopleIcon from "@mui/icons-material/People";
-import CloseIcon from "@mui/icons-material/Close";
-import Avatar from "@mui/icons-material/Avatar";
+	Drawer,
+	List,
+	ListItem,
+	ListItemText,
+	ListItemIcon,
+	IconButton,
+} from '@material-ui/core';
+import MenuIcon from '@mui/icons-material/Menu';
+import HomeIcon from '@mui/icons-material/Home';
+import PeopleIcon from '@mui/icons-material/People';
+import CloseIcon from '@mui/icons-material/Close';
+import Avatar from '@mui/material/Avatar';
 
-import DescriptionIcon from "@mui/icons-material/Description";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import { useHistory } from "react-router-dom";
-import ok from "../ok.png";
+import DescriptionIcon from '@mui/icons-material/Description';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { useHistory } from 'react-router-dom';
+import ok from '../ok.png';
 
 const DrawerComponent = ({ user, handleLogout }) => {
-  const [openDrawer, setOpenDrawer] = useState(false);
-  const history = useHistory();
+	const [openDrawer, setOpenDrawer] = useState(false);
+	const history = useHistory();
 
-  const handleClick = (item) => {
-    history.push(item.path);
-    setOpenDrawer(!openDrawer);
-  };
-  const menuItems = [
-    {
-      text: "HOME",
-      icon: <HomeIcon />,
-      path: "/",
-    },
-    {
-      text: "BLOGS",
-      icon: <DescriptionIcon />,
-      path: "/blogs",
-    },
-    {
-      text: "USERS",
-      icon: <PeopleIcon />,
-      path: "/users",
-    },
-  ];
-  const login = [
-    {
-      text: "LOGIN",
-      icon: <AccountCircleIcon />,
-      path: "/login",
-    },
-  ];
-  const logout = [
-    {
-      text: "LOGOUT",
-      icon: <CloseIcon />,
-      path: "/",
-    },
-  ];
-  return (
-    <>
-      <Drawer
-        anchor="right"
-        onClose={() => setOpenDrawer(false)}
-        open={openDrawer}
-      >
-        <List>
-          {menuItems.map((item) => {
-            return (
-              <ListItem
-                divider={true}
-                button
-                key={item.text}
-                onClick={() => handleClick(item)}
-              >
-                <ListItemIcon>{item.icon}</ListItemIcon>
-                <ListItemText className={classes.link}>
-                  {item.text}
-                </ListItemText>
-              </ListItem>
-            );
-          })}
-        </List>
-        <List>
-          {!user
-            ? login.map((item) => {
-                return (
-                  <ListItem
-                    divider={true}
-                    button
-                    key={item.text}
-                    onClick={() => handleClick(item)}
-                  >
-                    <ListItemIcon>{item.icon}</ListItemIcon>
-                    <ListItemText>{item.text}</ListItemText>
-                  </ListItem>
-                );
-              })
-            : logout.map((item) => {
-                return (
-                  <ListItem divider={true} button onClick={handleLogout}>
-                    <ListItemIcon>{item.icon}</ListItemIcon>
-                    <ListItemText>{item.text}</ListItemText>
-                  </ListItem>
-                );
-              })}
-        </List>
-      </Drawer>
+	const handleClick = (item) => {
+		history.push(item.path);
+		setOpenDrawer(!openDrawer);
+	};
+	const menuItems = [
+		{
+			text: 'HOME',
+			icon: <HomeIcon />,
+			path: '/',
+		},
+		{
+			text: 'BLOGS',
+			icon: <DescriptionIcon />,
+			path: '/blogs',
+		},
+		{
+			text: 'USERS',
+			icon: <PeopleIcon />,
+			path: '/users',
+		},
+	];
+	const login = [
+		{
+			text: 'LOGIN',
+			icon: <AccountCircleIcon />,
+			path: '/login',
+		},
+	];
+	const logout = [
+		{
+			text: 'LOGOUT',
+			icon: <CloseIcon />,
+			path: '/',
+		},
+	];
+	return (
+		<>
+			<Drawer
+				anchor="right"
+				onClose={() => setOpenDrawer(false)}
+				open={openDrawer}
+			>
+				<List>
+					{menuItems.map((item) => {
+						return (
+							<ListItem
+								divider={true}
+								button
+								key={item.text}
+								onClick={() => handleClick(item)}
+							>
+								<ListItemIcon>{item.icon}</ListItemIcon>
+								<ListItemText className={classes.link}>
+									{item.text}
+								</ListItemText>
+							</ListItem>
+						);
+					})}
+				</List>
+				<List>
+					{!user
+						? login.map((item) => {
+								return (
+									<ListItem
+										divider={true}
+										button
+										key={item.text}
+										onClick={() => handleClick(item)}
+									>
+										<ListItemIcon>{item.icon}</ListItemIcon>
+										<ListItemText>{item.text}</ListItemText>
+									</ListItem>
+								);
+						  })
+						: logout.map((item) => {
+								return (
+									<ListItem divider={true} button onClick={handleLogout}>
+										<ListItemIcon>{item.icon}</ListItemIcon>
+										<ListItemText>{item.text}</ListItemText>
+									</ListItem>
+								);
+						  })}
+				</List>
+			</Drawer>
 
-      <IconButton onClick={() => setOpenDrawer(!openDrawer)}>
-        <MenuIcon anchor="right" />
-      </IconButton>
-      {user ? <Avatar alt="OK" src={ok} /> : null}
-    </>
-  );
+			<IconButton onClick={() => setOpenDrawer(!openDrawer)}>
+				<MenuIcon anchor="right" />
+			</IconButton>
+			{user ? <Avatar alt="OK" src={ok} /> : null}
+		</>
+	);
 };
 
 export default DrawerComponent;
