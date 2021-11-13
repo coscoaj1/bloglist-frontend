de0d81521d276c35b963ce2731752771a1a75028
coscoaj1 <coscoanthony1@gmail.com>
2021-11-13 08:41:09 -0500

    responsive navbar

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,48 +1,50 @@
-import Link from '@mui/material/Link';
-import * as React from 'react';
-import CssBaseline from '@mui/material/CssBaseline';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
+import Link from "@mui/material/Link";
+import * as React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
 
 function Copyright() {
-	return (
-		<Typography variant="body2" color="text.secondary">
-			{'Copyright © '}
-			<Link color="inherit" href="https://mui.com/">
-				Your Website
-			</Link>{' '}
-			{new Date().getFullYear()}
-			{'.'}
-		</Typography>
-	);
+  return (
+    <Typography variant="body2" color="secondary">
+      {"Copyright © "}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
 }
 export default function Footer() {
-	return (
-		<Box
-			component="footer"
-			sx={{
-				bgcolor: 'primary.main',
-				position: 'fixed',
-				bottom: 0,
-				left: 0,
-				mt: 4,
-				width: '100%',
-				py: 2,
-			}}
-		>
-			<Container maxWidth="sm">
-				<Link component="button">
-					<Typography variant="body1">Link</Typography>
-				</Link>
-				<Link component="button">
-					<Typography>Terms</Typography>
-				</Link>
-				<Link component="button">
-					<Typography>Privacy</Typography>
-				</Link>
-				<Copyright />
-			</Container>
-		</Box>
-	);
+  return (
+    <Box
+      component="footer"
+      sx={{
+        bgcolor: "primary.main",
+        position: "fixed",
+        bottom: 0,
+        left: 0,
+        mt: 4,
+        width: "100%",
+        py: 2,
+      }}
+    >
+      <Container maxWidth="sm">
+        <Link component="button">
+          <Typography variant="body1" color="inherit">
+            Link
+          </Typography>
+        </Link>
+        <Link component="button">
+          <Typography>Terms</Typography>
+        </Link>
+        <Link component="button">
+          <Typography>Privacy</Typography>
+        </Link>
+        <Copyright />
+      </Container>
+    </Box>
+  );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,100 +1,78 @@
-import React from 'react';
-import { styled } from '@mui/material/styles';
-import Toolbar from '@mui/material/Toolbar';
-import AppBar from '@mui/material/AppBar';
-import Button from '@mui/material/Button';
-import PestControlRodentIcon from '@mui/icons-material/PestControlRodent';
-import DrawerComponent from './DrawerComponent';
-import useMediaQuery from '@mui/material/useMediaQuery';
-import { Link } from 'react-router-dom';
-import Typography from '@mui/material/Typography';
-import Avatar from '@mui/material/Avatar';
-import ok from '../ok.png';
-import { useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-// const useStyles = makeStyles({
-// 	root: {
-// 		display: 'flex',
-// 	},
-// 	orange: {
-// 		height: '60px',
-// 		width: '60px',
-// 		margin: '-.3em'
-// 	},
-// 	toolLeft: {
-// 		width: '100%'
-// 	},
-// 	toolRight: {
-// 		display: 'flex',
-// 		width: '30%',
-// 		gap: '1em'
-
-// 	},
-
-// });
+import React from "react";
+import LibraryBooks from "@mui/icons-material/LibraryBooks";
+import Toolbar from "@mui/material/Toolbar";
+import AppBar from "@mui/material/AppBar";
+import Button from "@mui/material/Button";
+import DrawerComponent from "./DrawerComponent";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { Link } from "react-router-dom";
+import Typography from "@mui/material/Typography";
+import Avatar from "@mui/material/Avatar";
+import ok from "../ok.png";
+import { useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
 
 export default function NavBar({ user, handleLogout }) {
-	const theme = useTheme();
-	const isMatch = useMediaQuery(theme.breakpoints.down('xs'));
+  // const theme = useTheme();
+  // const isMatch = useMediaQuery(theme.breakpoints.down("xs"));
 
-	return (
-		<>
-			<AppBar position="fixed" sx={{ width: '100vw', margin: 0 }}>
-				<Toolbar
-					sx={{
-						display: 'flex',
-						justifyContent: 'space-between',
-						alignItems: 'center',
-					}}
-				>
-					<Box sx={{ display: 'flex' }}>
-						<Button sx={{ mr: 2 }} size="large" component={Link} to="/">
-							<PestControlRodentIcon
-								fontSize="large"
-								sx={{
-									color: 'gray',
-									fontSize: 40,
-								}}
-							/>
-						</Button>
-						<Typography sx={{ paddingRight: 2, paddingTop: 1 }} variant="h4">
-							Catlify
-						</Typography>
+  return (
+    <>
+      <AppBar position="fixed" sx={{ width: "100vw", margin: 0 }}>
+        <Toolbar
+          component="nav"
+          sx={{
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <Box sx={{ display: { xs: `none`, md: `flex` } }}>
+            <Button sx={{ mr: 2 }} size="large" component={Link} to="/">
+              <LibraryBooks
+                color="secondary"
+                sx={{ width: "40px", height: "40px" }}
+              />
+            </Button>
+            <Typography sx={{ paddingRight: 2, paddingTop: 1 }} variant="h4">
+              BlogReviews
+            </Typography>
 
-						<Button color="inherit" component={Link} to="/blogs">
-							blogs
-						</Button>
-						<Button
-							color="inherit"
-							component="div"
-							sx={{ flexGrow: 1 }}
-							component={Link}
-							to="/users"
-						>
-							users
-						</Button>
-					</Box>
-					{user ? (
-						<Toolbar
-							sx={{ display: 'flex', justifyContent: 'center', gap: '1rem' }}
-						>
-							<Button color="inherit" size="small" onClick={handleLogout}>
-								logout
-							</Button>
-							<Typography sx={{ paddingBottom: '3px' }}>{user.name}</Typography>
-							<Avatar alt="OK" src={ok} />
-						</Toolbar>
-					) : (
-						<Button color="inherit" component={Link} to="/login">
-							login
-						</Button>
-					)}
-				</Toolbar>
-				{isMatch ? (
-					<DrawerComponent handleLogout={handleLogout} user={user} />
-				) : null}
-			</AppBar>
-			<Toolbar />
-		</>
-	);
+            <Button color="inherit" component={Link} to="/blogs">
+              blogs
+            </Button>
+            <Button
+              color="inherit"
+              component="div"
+              sx={{ flexGrow: 1 }}
+              component={Link}
+              to="/users"
+            >
+              users
+            </Button>
+          </Box>
+          {user ? (
+            <Toolbar
+              sx={{
+                display: { xs: `none`, md: `flex` },
+                justifyContent: "center",
+                gap: "1rem",
+              }}
+            >
+              <Button color="inherit" size="small" onClick={handleLogout}>
+                logout
+              </Button>
+              <Typography sx={{ paddingBottom: "3px" }}>{user.name}</Typography>
+              <Avatar alt="OK" src={ok} />
+            </Toolbar>
+          ) : (
+            <Button color="inherit" component={Link} to="/login">
+              login
+            </Button>
+          )}
+        </Toolbar>
+        <DrawerComponent handleLogout={handleLogout} user={user} />
+      </AppBar>
+      <Toolbar />
+    </>
+  );
 }
