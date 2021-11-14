61b853e4b019f6e5d7b0bd170ab3adb5cfd54fe2
coscoaj1 <coscoanthony1@gmail.com>
2021-11-14 11:39:11 -0500

    connectd routes to backend

--- src/services/users.js
+++ src/services/users.js
@@ -1,10 +1,10 @@
-import axios from 'axios';
-const baseUrl = '/api/users';
+import axios from "axios";
+const baseUrl = "https://blogreview-backend.herokuapp.com/api/users";
 
 const getUsers = async () => {
-	const response = await axios.get(baseUrl);
-	console.log(response.data);
-	return response.data;
+  const response = await axios.get(baseUrl);
+  console.log(response.data);
+  return response.data;
 };
 
 export default { getUsers };
