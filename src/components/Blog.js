40be4a0af4a994813a02f77542cb6a836d773e35
Anthony Cosco <coscoanthony1@gmail.com>
2021-11-14 10:42:29 -0500

    redirect to home page upon deleting blog

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,130 +1,108 @@
-import { React, useState } from "react";
-import "../Index.css";
-import { useParams } from "react-router";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import Typography from "@mui/material/Typography";
-import ThumbUpIcon from "@mui/icons-material/ThumbUp";
-import DeleteIcon from "@mui/icons-material/Delete";
-import ModeCommentIcon from "@mui/icons-material/ModeComment";
-import TextField from "@mui/material/TextField";
-import { IconButton, CardActions, Grid } from "@mui/material/";
+import { React, useState } from 'react';
+import '../Index.css';
+import { useParams } from 'react-router';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import DeleteIcon from '@mui/icons-material/Delete';
+import ModeCommentIcon from '@mui/icons-material/ModeComment';
+import TextField from '@mui/material/TextField';
+import { IconButton, CardActions, Grid } from '@mui/material/';
+import { useHistory } from 'react-router-dom';
 
-// const useStyles = makeStyles(() => ({
-// 	root: {
-// 		minWidth: 275,
-// 		maxWidth: 345,
-// 		backgroundColor: '#424242',
-// 		color: 'white',
-// 	},
-// 	title: {
-// 		fontSize: 22,
-
-// 	},
-// 	field: {
-// 		marginTop: 20,
-// 	},
-// 	grid: {
-// 		width: '100%',
-// 		justifyContent: 'center',
-// 	},
-// }));
 const Blog = ({ blogs, handleLike, createComment, handleDelete }) => {
-  const [expanded, setExpanded] = useState(false);
-  const [newComment, setNewComment] = useState([]);
+	const [expanded, setExpanded] = useState(false);
+	const [newComment, setNewComment] = useState([]);
 
-  const handleExpandedClick = () => {
-    setExpanded(!expanded);
-  };
+	let history = useHistory();
 
-  const handleChange = (event) => {
-    setNewComment(event.target.value);
-  };
+	const handleExpandedClick = () => {
+		setExpanded(!expanded);
+	};
 
-  const id = useParams().id;
-  const blog = blogs.find((b) => b.id === String(id));
+	const handleChange = (event) => {
+		setNewComment(event.target.value);
+	};
 
-  const addComment = (event) => {
-    event.preventDefault();
-    createComment({
-      comment: newComment,
-      id: blog.id,
-    });
-    setNewComment("");
-  };
-  if (!blog) {
-    return null;
-  }
+	const id = useParams().id;
+	const blog = blogs.find((b) => b.id === String(id));
 
-  return (
-    <div>
-      <div>
-        <Card sx={{ maxWidth: 345 }} elevation={12}>
-          <CardContent>
-            <Typography fontWeight="medium" variant="h5">
-              {blog.title}
-            </Typography>{" "}
-            <Typography>{blog.author}</Typography>
-            <a href="">
-              <Typography>{blog.url}</Typography>
-            </a>
-            <Typography fontWeight="lighter">{blog.likes} likes</Typography>
-            <CardActions
-              sx={{
-                width: "100%",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-              }}
-            >
-              <IconButton
-                id="likeButton"
-                size="small"
-                onClick={() => handleLike(blog)}
-              >
-                <ThumbUpIcon />
-              </IconButton>
-              <IconButton onClick={() => handleDelete(blog)}>
-                <DeleteIcon />
-              </IconButton>
-              <IconButton onClick={handleExpandedClick}>
-                <ModeCommentIcon />
-              </IconButton>
-            </CardActions>
-          </CardContent>
-        </Card>
-        {expanded ? (
-          <form noValidate autoComplete="off" onSubmit={addComment}>
-            <TextField
-              onChange={handleChange}
-              value={newComment}
-              color="secondary"
-              variant="outlined"
-              label="enter comment"
-              fullWidth
-              sx={{ marginTop: "2rem" }}
-            />
-          </form>
-        ) : null}
-      </div>
-      <div>
-        <Typography variant="h4">comments</Typography>
-        {blog.comments.map((comment) => {
-          return <div key={comment.comments}>{comment.comments}</div>;
-        })}
-      </div>
-    </div>
-  );
+	const addComment = (event) => {
+		event.preventDefault();
+		createComment({
+			comment: newComment,
+			id: blog.id,
+		});
+		setNewComment('');
+	};
+	if (!blog) {
+		return null;
+	}
+
+	return (
+		<div>
+			<div>
+				<Card sx={{ maxWidth: 345 }} elevation={12}>
+					<CardContent>
+						<Typography fontWeight="medium" variant="h5">
+							{blog.title}
+						</Typography>{' '}
+						<Typography>{blog.author}</Typography>
+						<a href="">
+							<Typography>{blog.url}</Typography>
+						</a>
+						<Typography fontWeight="lighter">{blog.likes} likes</Typography>
+						<CardActions
+							sx={{
+								width: '100%',
+								display: 'flex',
+								alignItems: 'center',
+								justifyContent: 'center',
+							}}
+						>
+							<IconButton
+								id="likeButton"
+								size="small"
+								onClick={() => handleLike(blog)}
+							>
+								<ThumbUpIcon />
+							</IconButton>
+							<IconButton
+								onClick={() => {
+									handleDelete(blog), history.push('/');
+								}}
+							>
+								<DeleteIcon />
+							</IconButton>
+							<IconButton onClick={handleExpandedClick}>
+								<ModeCommentIcon />
+							</IconButton>
+						</CardActions>
+					</CardContent>
+				</Card>
+				{expanded ? (
+					<form noValidate autoComplete="off" onSubmit={addComment}>
+						<TextField
+							onChange={handleChange}
+							value={newComment}
+							color="secondary"
+							variant="outlined"
+							label="enter comment"
+							fullWidth
+							sx={{ marginTop: '2rem' }}
+						/>
+					</form>
+				) : null}
+			</div>
+			<div>
+				<Typography variant="h4">comments</Typography>
+				{blog.comments.map((comment) => {
+					return <div key={comment.comments}>{comment.comments}</div>;
+				})}
+			</div>
+		</div>
+	);
 };
 
 export default Blog;
-
-// 	like
-// <Button
-// 	id="removeButton"
-// 	size="small"
-// 	color="primary"
-// 	onClick={() => handleDelete(blog)}
-// >
-// 	remove
-// </Button>
