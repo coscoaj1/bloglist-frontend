3d9421f81d80c5f2340cd3c7a48f87f320801ff5
Anthony Cosco <coscoanthony1@gmail.com>
2021-11-25 08:32:51 -0500

    iconbutton gray color

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,56 +1,56 @@
-import { Container, Grid } from "@mui/material";
-import { ReactComponent as MySvg } from "../assets/undraw_blog_post_re_fy5x.svg";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import { Link } from "react-router-dom";
+import { Container, Grid } from '@mui/material';
+import { ReactComponent as MySvg } from '../assets/undraw_blog_post_re_fy5x.svg';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom';
 const AboutSection = ({ user }) => {
-  return (
-    <Container
-      component="section"
-      maxWidth="lg"
-      sx={{
-        backgroundColor: "#FFFFFF",
-        mb: 15,
-        borderRadius: "8px",
-      }}
-    >
-      <Grid container spacing={3}>
-        <Grid
-          item
-          xs={12}
-          sm={6}
-          container
-          flexDirection="column"
-          justifyContent="center"
-          alignItems="center"
-        >
-          <MySvg width={350} height={500} />
-        </Grid>
-        <Grid
-          item
-          xs={12}
-          sm={6}
-          container
-          flexDirection="column"
-          justifyContent="center"
-          alignItems="center"
-          sx={{ mx: { xs: 12, sm: 0 } }}
-        >
-          {" "}
-          <Typography
-            variant="h4"
-            fontWeight="bold"
-            textAlign="center"
-            gutterBottom
-          >
-            Welcome to Blog Reviews.
-          </Typography>
-          <Typography variant="h5" textAlign="center" sx={{ color: "gray" }}>
-            {user
-              ? `Submit your favorite blogs below. Also like and comment on other blogs.`
-              : `Login to share your favorite blog posts. You may also like and comment on other posts.`}
-          </Typography>
-          {user ? (
+	return (
+		<Container
+			component="section"
+			maxWidth="lg"
+			sx={{
+				backgroundColor: '#FFFFFF',
+				mb: 15,
+				borderRadius: '8px',
+			}}
+		>
+			<Grid container spacing={3}>
+				<Grid
+					item
+					xs={12}
+					sm={6}
+					container
+					flexDirection="column"
+					justifyContent="center"
+					alignItems="center"
+				>
+					<MySvg width={350} height={500} />
+				</Grid>
+				<Grid
+					item
+					xs={12}
+					sm={6}
+					container
+					flexDirection="column"
+					justifyContent="center"
+					alignItems="center"
+					sx={{ mx: { xs: 12, sm: 0 } }}
+				>
+					{' '}
+					<Typography
+						variant="h4"
+						fontWeight="bold"
+						textAlign="center"
+						gutterBottom
+					>
+						Welcome to Blog Reviews.
+					</Typography>
+					<Typography variant="h5" textAlign="center" sx={{ color: 'gray' }}>
+						{user
+							? `Submit your favorite blogs below. Also like and comment on other blogs.`
+							: `Login to share your favorite blog posts. You may also like and comment on other posts.`}
+					</Typography>
+					{/* {user ? (
             <Button
               sx={{ my: 2 }}
               component={Link}
@@ -68,11 +68,11 @@ const AboutSection = ({ user }) => {
             >
               Login
             </Button>
-          )}
-        </Grid>
-      </Grid>
-    </Container>
-  );
+          )} */}
+				</Grid>
+			</Grid>
+		</Container>
+	);
 };
 
 export default AboutSection;
